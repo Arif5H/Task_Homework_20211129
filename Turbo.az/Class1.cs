297c7327e8405d236a41c8d0fbcda999e66f433e
Arif5H <arifhh@code.edu.az>
2021-12-09 21:15:40 +0400

    Добавьте файлы проекта.

---
+++ Turbo.az/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turbo.az
+{
+    internal class Car
+    {
+        public string Model;
+        public string Brand;
+        public int Year;
+        public string Location;
+        public double Price;
+        
+        public string GetCarInfo()
+        {
+            return $"{Model}. {Brand}, {Year}, {Location}, {Price}";
+        }
+
+
+    }
+}
---
+++ Turbo.az/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Turbo.az
+{
+    class Car_Store
+    {
+        public static void Main(string[] args)
+        {/*turbo.az     saytina daxil olub orada bir Car modelinin tesvirini arashdirin
+          * Sonra 4 xususiyyeti olacq sekilde bir Car classı yaradın
+          * marka
+          * model
+          * year
+          * price
+          * son dersdeki kimi istifadeciden say saorushulsun
+          * ve o qeder sayda mashini elave etmek(butun gostericilerini)*/
+
+            Console.Write("Number of Cars ");
+            int Carsquantity = Convert.ToInt32(Console.ReadLine());
+
+            Car[] Cars = new Car [Carsquantity];
+
+            for(int i = 0; i< Cars.Length; i++)
+            {
+                Car Car1 = new Car();
+
+                Console.Write($"Car's Model {i + 1}: ");
+                Car1.Model = Console.ReadLine();
+
+                Console.Write($"Car's Brand {i + 1}: ");
+                Car1.Brand = Console.ReadLine();
+
+                Console.Write($"Car's Year  {i + 1}: ");
+                Car1.Year = Convert.ToInt32 (Console.ReadLine());
+
+                Console.Write($"Car's Location {i + 1}: ");
+                Car1.Location = Console.ReadLine();
+
+                Console.Write($"Car's Price {i + 1}: ");
+                Car1.Price = Convert.ToDouble (Console.ReadLine());
+                Cars[i] = Car1;
+
+
+            }
+
+            Console.WriteLine("Cars Info: ");
+            foreach (var Car in Cars)
+            {
+                string name = Car.GetCarInfo();
+
+                Console.WriteLine(name + $"\t");
+            }
+
+        }
+    }
+}
